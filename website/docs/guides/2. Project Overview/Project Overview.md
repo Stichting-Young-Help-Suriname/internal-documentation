651e680a092b9814ab0c39dbe68d104e5cf682a7
Youri K. <yourikarijo1405@gmail.com>
2025-07-12 22:32:16 -0300

    feat: setup docusaurus

---
+++ website/docs/guides/2. Project Overview/Project Overview.md
@@ -0,0 +1,8 @@
+**Project Name**: YHS Math Game 2.0
+
+**Introduction:** 
+In 2025, Foundation Young Help Suriname released the first version of its Math Game in collaboration with IDB Lab and the Ministry of Education as part of its HYPE (Helping Youth Progress in Education) program. This game was aimed as a way for students in grade 10 (2e MULO) to be able to practice and test their math skills in the areas of triangles, averages and fractions using their mobile devices. The game was released in conjunction with a YouTube channel that has videos about the different topics addressed in the game.
+
+For a second iteration of this game, the development team will consist of motivated students and/or young professionals who are still building their real-world skills. This project will not only deliver an improved learning tool, but it will also showcase that there is talent and experience available amongst our own team to be able to develop more of these types of projects in the future.
+
+**Target Platform:** Mobile devices (Android + iOS)
---
+++ website/docusaurus.config.ts
@@ -0,0 +1,144 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'YHS Math Game 2.0',
+  tagline: 'Internal Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://Stichting-Young-Help-Suriname.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/internal-documentation',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Stichting-Young-Help-Suriname', // Usually your GitHub org/user name.
+  projectName: 'internal-documentation', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'nl'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/docs',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Stichting-Young-Help-Suriname/internal-documentation/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'YHS Math Game 2.0',
+      logo: {
+        alt: 'YHS Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docs',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'guides',
+          position: 'left',
+          label: 'Guides',
+        },
+        {
+          href: 'https://github.com/Stichting-Young-Help-Suriname/internal-documentation',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/docs/docs',
+            },
+            {
+              label: 'Guides',
+              to: '/docs/guides',
+            },
+          ],
+        },
+        // {
+        //   title: 'Community',
+        //   items: [
+        //     {
+        //       label: 'Stack Overflow',
+        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //     },
+        //     {
+        //       label: 'Discord',
+        //       href: 'https://discordapp.com/invite/docusaurus',
+        //     },
+        //     {
+        //       label: 'X',
+        //       href: 'https://x.com/docusaurus',
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: '/blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Stichting Young Help Suriname, Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
